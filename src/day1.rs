157a4960ad6b8f2d78e3ec7b2eae5bf8d59e8092
abrackx <abrackx@gmail.com>
2022-12-01 21:24:54 -0500

    discovered if let

--- src/day1.rs
+++ src/day1.rs
@@ -13,13 +13,15 @@ pub(crate) fn part1() {
     let mut temp_sum = 0;
     let mut max_cal = 0;
     for line in reader.lines() {
-        if line.as_ref().unwrap().is_empty() {
-            temp_sum = 0;
-            continue;
-        }
-        temp_sum += line.unwrap().parse::<i32>().unwrap();
-        if temp_sum > max_cal {
-            max_cal = temp_sum;
+        if let Ok(cal) = line {
+            if cal.is_empty() {
+                temp_sum = 0;
+                continue;
+            }
+            temp_sum += cal.parse::<i32>().unwrap();
+            if temp_sum > max_cal {
+                max_cal = temp_sum;
+            }
         }
     }
     println!("Day 1 Part 1: {:?}", max_cal);
@@ -30,19 +32,23 @@ pub(crate) fn part2() {
     let mut temp_sum = 0;
     let mut max_cal: BinaryHeap<Reverse<i32>> = BinaryHeap::with_capacity(3);
     for line in reader.lines() {
-        if line.as_ref().unwrap().is_empty() {
-            temp_sum = 0;
-            continue;
-        }
-        temp_sum += line.unwrap().parse::<i32>().unwrap();
-        if temp_sum > max_cal.peek().unwrap_or(&Reverse(0)).0 {
-            if max_cal.len() == 3 {
-                max_cal.pop();
+        if let Ok(maybe_cal) = line {
+            if maybe_cal.is_empty() {
+                temp_sum = 0;
+                continue;
             }
-            max_cal.push(Reverse(temp_sum));
+            if let Ok(cal) = maybe_cal.parse::<i32>() {
+                temp_sum += cal;
+                if temp_sum > max_cal.peek().unwrap_or(&Reverse(0)).0 {
+                    if max_cal.len() == 3 {
+                        max_cal.pop();
+                    }
+                    max_cal.push(Reverse(temp_sum));
+                }
+            }
         }
     }
     let mut sum = 0;
-    max_cal.into_iter().for_each(|cal| sum += cal.0 );
+    max_cal.into_iter().for_each(|cal| sum += cal.0);
     println!("Day 1 Part 2: {:?}", sum);
 }
