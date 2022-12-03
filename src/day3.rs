202c888e7a1aa8aa5276df13f5b95db287716f19
abrackx <abrackx@gmail.com>
2022-12-03 10:57:03 -0500

    day 3 part 2 solution

--- src/day3.rs
+++ src/day3.rs
@@ -27,3 +27,26 @@ pub(crate) fn part1() {
     }
     println!("Day 3 Part 1: {:?}", sum);
 }
+
+pub(crate) fn part2() {
+    let reader = setup();
+    let mut lines = reader.lines();
+    let mut sum = 0;
+    while let (Some(line1), Some(line2), Some(line3)) = (lines.next(), lines.next(), lines.next()) {
+        if let (Ok(list1), Ok(list2), Ok(list3)) = (line1, line2, line3) {
+            let list1_set: HashSet<char> = list1.chars().collect();
+            let list2_set: HashSet<char> = list2.chars().collect();
+            let list3_set: HashSet<char> = list3.chars().collect();
+            let sets = &vec![list1_set, list2_set, list3_set];
+            let intersect = sets[0].iter().filter (move |c| sets[1..].iter().all (|s| s.contains (c))).collect::<Vec<_>>();
+            if let Some(common_item) = intersect.get(0) {
+                if common_item.is_uppercase() {
+                    sum += **common_item as u32 % 32 + 26;
+                } else {
+                    sum += **common_item as u32 % 32;
+                }
+            }
+        }
+    }
+    println!("Day 3 Part 2: {:?}", sum);
+}
